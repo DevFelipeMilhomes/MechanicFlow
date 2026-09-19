5a522460eb2f9e01d987f5d12b1ccd2f83b67daa
Felipe <f3lipe@archlinux.localdomain>
2026-09-19 09:31:50 -0300

    feat: adding service order, stock, stock movement e vehicle repositories

---
+++ backend/src/main/java/com/devfelipemilhomes/serviceOrder/ServiceOrderRepository.java
@@ -0,0 +1,6 @@
+package com.devfelipemilhomes.serviceOrder;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServiceOrderRepository extends JpaRepository<ServiceOrder, Long> {
+}
---
+++ backend/src/main/java/com/devfelipemilhomes/stockMoviment/StockMovimentRepository.java
@@ -0,0 +1,6 @@
+package com.devfelipemilhomes.stockMoviment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StockMovimentRepository extends JpaRepository<StockMoviment, Long> {
+}
