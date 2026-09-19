cb459ad3dd124187967a58606569e90adb71b6e1
Felipe <f3lipe@archlinux.localdomain>
2026-09-18 22:08:24 -0300

    feat: adding client, part, professional and service repositories

---
+++ backend/src/main/java/com/devfelipemilhomes/professional/ProfessionalRepository.java
@@ -0,0 +1,6 @@
+package com.devfelipemilhomes.professional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfessionalRepository extends JpaRepository<Professional, Long> {
+}
